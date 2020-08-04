322b405e8e80dfb8dc0aa4fb23ef0c2980e133bf
Vladislav <novitsky.vladislav00@gmail.com>
2020-08-04 04:06:47 +0300

    State message widget

--- lib/main.dart
+++ lib/main.dart
@@ -23,19 +23,50 @@ class MyHomePage extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(
-        title: Text('Hey'),
-      ),
-      body: Column(
-        children: <Widget>[TestWidget(), TestWidget()],
-      ),
-    );
+        appBar: AppBar(
+          title: Text('Hey'),
+        ),
+        body: TextInputWidget());
   }
 }
 
-class TestWidget extends StatelessWidget {
+class TextInputWidget extends StatefulWidget {
+  @override
+  _TextInputWidgetState createState() => _TextInputWidgetState();
+}
+
+class _TextInputWidgetState extends State<TextInputWidget> {
+  final controller = TextEditingController();
+  String text = "";
+
+  @override
+  void dispose() {
+    super.dispose();
+    controller.dispose();
+  }
+
+  void changeText(text) {
+    if (text == 'Hello World!') {
+      controller.clear();
+      text = '';
+    }
+    setState(() {
+      this.text = text;
+    });
+  }
+
   @override
   Widget build(BuildContext context) {
-    return Text('Adele was here');
+    return Column(
+      children: <Widget>[
+        TextField(
+          controller: this.controller,
+          decoration: InputDecoration(
+              prefixIcon: Icon(Icons.message), labelText: "Type a message:"),
+          onChanged: (text) => this.changeText(text),
+        ),
+        Text(this.text)
+      ],
+    );
   }
 }
