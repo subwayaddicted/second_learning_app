a88ee5a25683e7cb5a1a28ae7a9052aeb6c7a911
Vladislav <novitsky.vladislav00@gmail.com>
2020-08-25 06:50:00 +0300

    Added Google auth

--- lib/login.dart
+++ lib/login.dart
@@ -1,5 +1,8 @@
+import 'package:firebase_auth/firebase_auth.dart';
 import 'package:flutter/material.dart';
 import 'package:second_learning_app/myHomePage.dart';
+
+import 'auth.dart';
 
 class LoginPage extends StatelessWidget {
   @override
@@ -18,38 +21,56 @@ class Body extends StatefulWidget {
 }
 
 class _BodyState extends State<Body> {
-  String name;
-  TextEditingController controller = new TextEditingController();
+  FirebaseUser user;
+
+  @override
+  void initState() {
+    super.initState();
+    signOutGoogle();
+  }
 
   void click() {
-    this.name = controller.text;
-    Navigator.push(
-        context,
-        MaterialPageRoute(
-          builder: (context) => MyHomePage(this.name),
-        ));
+    signInWithGoogle().then((user) => {
+          this.user = user,
+          Navigator.push(
+              context,
+              MaterialPageRoute(
+                builder: (context) => MyHomePage(user.displayName),
+              ))
+        });
+  }
+
+  Widget googleLoginButton() {
+    return OutlineButton(
+      onPressed: this.click,
+      shape: RoundedRectangleBorder(borderRadius: BorderRadius.circular(45)),
+      splashColor: Colors.grey,
+      borderSide: BorderSide(color: Colors.grey),
+      child: Padding(
+        padding: EdgeInsets.fromLTRB(0, 10, 0, 10),
+        child: Row(
+          mainAxisAlignment: MainAxisAlignment.center,
+          mainAxisSize: MainAxisSize.min,
+          children: [
+            Image(
+              image: AssetImage('assets/google_logo.png'),
+              height: 35,
+            ),
+            Padding(
+              padding: EdgeInsets.only(left: 10),
+              child: Text(
+                'Sign in with Google',
+                style: TextStyle(color: Colors.grey, fontSize: 25),
+              ),
+            )
+          ],
+        ),
+      ),
+    );
   }
 
   @override
   Widget build(BuildContext context) {
-    return Align(
-      alignment: Alignment.center,
-      child: Padding(
-          padding: EdgeInsets.all(10),
-          child: TextField(
-            controller: this.controller,
-            decoration: InputDecoration(
-                prefixIcon: Icon(Icons.person),
-                labelText: "Type your name:",
-                border: OutlineInputBorder(
-                    borderSide: BorderSide(width: 5, color: Colors.black54)),
-                suffixIcon: IconButton(
-                  icon: Icon(Icons.done),
-                  splashColor: Colors.deepOrange,
-                  tooltip: 'Submit',
-                  onPressed: this.click,
-                )),
-          )),
-    );
+    return Align(alignment: Alignment.center, child: googleLoginButton());
   }
 }
